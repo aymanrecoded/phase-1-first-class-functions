0d69cf5a279e311a865c9d917ad7847a4e0c0dc6
aymanattar <aymanxman19999@gmail.com>
2023-05-08 08:06:47 -0700

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction(callback) {
+  return callback();
+}
+
+function returnsANamedFunction() {
+  return function namedF() {
+  };
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+  };
+}
